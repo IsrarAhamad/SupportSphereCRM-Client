5d33483192de1cdb58d1a07517fbf925089b5e79
Israr8957 <ik6141333@gmail.com>
2026-07-17 16:55:16 +0530

    backend url added

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://supportspherecrm-server.onrender.com/api",
+  baseURL: "https://supportspherecrm-server-1.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
